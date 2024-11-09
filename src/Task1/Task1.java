f6023ff79ff396f3d21ea9a5966c5e65e81e4dbe
Vera Bylinskaia <iagyar@list.ru>
2024-11-09 23:49:24 +0300

    Задача 1

--- src/Task1/Task1.java
+++ src/Task1/Task1.java
@@ -1,13 +1,67 @@
 package Task1;
 
+import java.util.List;
+
 public class Task1 {
-//    Задание 1.
+    //    Задание 1.
 //    Реализовать, с учетом ооп подхода, приложение. Для проведения исследований с
 //    генеалогическим древом.
 //    Идея: описать некоторое количество компонент, например - модель человека и
 //    дерева. Под “проведением исследования” можно понимать например получение
 //    всех детей выбранного человека.
-public static void main(String[] args) {
 
-}
+    public static void main(String[] args) {
+//    int age;
+        FamilyTree familyTree = new FamilyTree();
+
+        // Создаем людей
+        Person ivan = new Person("Иванов", "Иван", "Сидорович", "12.11.1995");
+        Person petr = new Person("Иванов", "Петр", "Сидорович", 1992);
+        Person anna = new Person("Иванова", "Анна", "Владимировна", 1970);
+        Person sidor = new Person("Иванов", "Сидор", "Алексеевич", 1968);
+//    System.out.println(person);
+//    System.out.println(person1);
+//    System.out.println(person.getDateBirth());
+
+        // Устанавливаем родительские связи
+        // ivan
+        ivan.setMother(anna);
+        ivan.setFather(sidor);
+        anna.addChild(ivan);
+        sidor.addChild(ivan);
+
+        // petr
+        petr.setMother(anna);
+        petr.setFather(sidor);
+        anna.addChild(petr);
+        sidor.addChild(petr);
+
+        // Добавляем людей в древо
+        familyTree.addPerson(ivan);
+        familyTree.addPerson(petr);
+        familyTree.addPerson(anna);
+        familyTree.addPerson(sidor);
+
+        // Пример получения всех детей sidor
+        List<Person> sidorChildren = familyTree.getChildren(sidor);
+        for (Person child : sidorChildren) {
+            System.out.println("Дети Сидора: " + child.getName());
+
+
+        }
+
+        List<Person> annaChildren = familyTree.getChildren(anna);
+        for (Person child : annaChildren) {
+            System.out.println("Дети Анны: " + child.getName());
+
+
+        }
+
+
+        System.out.println(ivan);
+        System.out.println(sidor);
+        sidor.setDateBirth(24,5,1968);
+        System.out.println(sidor);
+
+    }
 }
